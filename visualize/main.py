82d773d489e21e65d8731769c5eab186dab96265
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-12-15 10:20:43 +0700

    fix: Use normal lines

--- visualize/main.py
+++ visualize/main.py
@@ -5,8 +5,8 @@ from typing import Dict, List, Tuple
 
 # Define constants for color mapping
 COLOR_CLEAN = '#00A9E0'
-COLOR_CLEAN_DIRTY = '#7A4B3B'
-COLOR_CLEAN_DIRTY_BEING_CLEANED = '#A4D7D3'
+COLOR_DIRTY = '#7A4B3B'
+COLOR_BEING_CLEANED = '#A4D7D3'
 
 def load_data(file_path: str) -> List[Dict]:
     """ Load the data from a JSON file and return it. """
@@ -42,10 +42,9 @@ def plot_data(hours: List[int], state_0: List[int], state_1: List[int], state_2:
     ax.set_ylim(bottom=0, top=stacked_state_2[-1])
 
     # Fill areas under the lines
-    ax.fill_between(hours, 0, state_0, color=COLOR_CLEAN, label='Clean', alpha=0.6)
-    ax.fill_between(hours, state_0, stacked_state_1, color=COLOR_CLEAN_DIRTY, label='Clean + Dirty', alpha=0.6)
-    ax.fill_between(hours, stacked_state_1, stacked_state_2, color=COLOR_CLEAN_DIRTY_BEING_CLEANED,
-                    label='Clean + Dirty + Being Cleaned', alpha=0.6)
+    ax.fill_between(hours, 0, state_0, color=COLOR_CLEAN, label='Clean', alpha=0.3)
+    ax.fill_between(hours, 0, state_1, color=COLOR_DIRTY, label='Dirty', alpha=0.3)
+    ax.fill_between(hours, 0, state_2, color=COLOR_BEING_CLEANED, label='Being Cleaned', alpha=0.3)
 
     # Adding labels and title
     ax.set_xlabel('Hour')
@@ -68,5 +67,5 @@ def main(file_path: str) -> None:
 
 # Run the script with the given file path
 if __name__ == "__main__":
-    file_path = "../logs/2024-12-15T09:35:20+07:00.json"
+    file_path = "../logs/2024-12-15T10:11:06+07:00.json"
     main(file_path)
