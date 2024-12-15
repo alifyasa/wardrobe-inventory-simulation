eefae76de7f68cd01b46a45b013bca1448d68490
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-12-15 08:56:51 +0700

    feat: Add readable time to logs

--- simulation/metrics.go
+++ simulation/metrics.go
@@ -9,6 +9,7 @@ import (
 
 type SimulationMetric struct {
 	Hour                int
+	ReadableTime        string
 	NumOfOutfitsByState map[outfits.OutfitState]int
 	NextEvent           string
 }
@@ -17,12 +18,13 @@ type SimulationMetrics []SimulationMetric
 
 func Track() {
 	SimulationState.Metrics = append(SimulationState.Metrics, SimulationMetric{
-		Hour: SimulationState.Hour,
+		Hour:         SimulationState.Hour,
+		ReadableTime: SimulationState.ReadableTime(),
+		NextEvent:    runtime.FuncForPC(reflect.ValueOf(SimulationState.Peek().EventFunc).Pointer()).Name(),
 		NumOfOutfitsByState: map[outfits.OutfitState]int{
 			outfits.CLEAN:         len(SimulationState.AllOutfits.GetAllOutfitsWithState(outfits.CLEAN)),
 			outfits.DIRTY:         len(SimulationState.AllOutfits.GetAllOutfitsWithState(outfits.DIRTY)),
 			outfits.BEING_CLEANED: len(SimulationState.AllOutfits.GetAllOutfitsWithState(outfits.BEING_CLEANED)),
 		},
-		NextEvent: runtime.FuncForPC(reflect.ValueOf(SimulationState.Peek().EventFunc).Pointer()).Name(),
 	})
 }
